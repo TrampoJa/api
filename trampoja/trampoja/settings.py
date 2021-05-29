26f7262512b71d266bde3f69e01fe8c429add124
Luiz Dendena <luiz.dendena@gmail.com>
2021-05-29 10:07:49 -0300

    Ajuste settings.py

--- trampoja/trampoja/settings.py
+++ trampoja/trampoja/settings.py
@@ -23,9 +23,9 @@ SECRET_KEY = 'tn!4a4qgooym1--qqtsf*)a3_veuggmhogkejiv!ey=p8xwtov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'beta.trampoja.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
